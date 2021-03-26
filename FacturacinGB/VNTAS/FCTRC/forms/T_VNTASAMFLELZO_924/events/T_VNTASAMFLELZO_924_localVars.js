1675299a2332b3b8e34aa4d730af6ab28121ecf7
Dorian Ramos <dorian.ramos.ec@gmail.com>
2021-03-26 16:41:51 -0500

    eliminar formulario y query erroneo

--- FacturacinGB/VNTAS/FCTRC/forms/T_VNTASAMFLELZO_924/events/T_VNTASAMFLELZO_924_localVars.js
+++
@@ -1,1 +0,0 @@
-/* variables locales de T_VNTASAMFLELZO_924*/
--- FacturacinGB/VNTAS/FCTRC/forms/T_VNTASAMFLELZO_924/events/executeQuery/Q_PRODDROD_BC88.js
+++
@@ -1,6 +0,0 @@
-
-//ProductoDRAQuery_BC88 Entity: ProductoDRA
-task.executeQuery.Q_PRODDROD_BC88 = function(executeQueryEventArgs){
-    executeQueryEventArgs.commons.execServer = true;
-    //executeQueryEventArgs.commons.serverParameters.ProductoDRA = true;
-};
