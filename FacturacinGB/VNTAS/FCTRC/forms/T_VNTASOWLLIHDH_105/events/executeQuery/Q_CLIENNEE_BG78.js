a0c9993a4e6918e80f744794ac2d365f1492e848
bapilco <bryanpilco.1597@gmail.com>
2021-03-27 13:40:09 -0500

    se agregan el grid para buscar el cliente por nombre

--- FacturacinGB/VNTAS/FCTRC/forms/T_VNTASOWLLIHDH_105/events/executeQuery/Q_CLIENNEE_BG78.js
+++ FacturacinGB/VNTAS/FCTRC/forms/T_VNTASOWLLIHDH_105/events/executeQuery/Q_CLIENNEE_BG78.js
@@ -1,6 +1,8 @@
 
 //ClienteQuery_BG78 Entity: Cliente
 task.executeQuery.Q_CLIENNEE_BG78 = function(executeQueryEventArgs){
-    executeQueryEventArgs.commons.execServer = false;
+    executeQueryEventArgs.commons.execServer = true;
     //executeQueryEventArgs.commons.serverParameters.Cliente = true;
+    
+    executeQueryEventArgs.parameters.nombreBusqueda = executeQueryEventArgs.commons.api.vc.model.ClienteB.nombre
 };
